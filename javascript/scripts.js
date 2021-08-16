cf7cd12878e97c2b58d8fa0a42da6a50b4d77391
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-08-16 15:42:29 +0200

    only one project active

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,65 +1,34 @@
 "use strict";
-/*
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelectorAll(".open-modal");
+
+const images = document.querySelector(".project");
+const modals = document.querySelector(".project__text");
 
 const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", openModal);
-}
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
-*/
-/*
-const sendMail = function () {
-  mail.href = "mailto:mikael_larsson84@hotmail.com";
-  mail.click("button");
-};
-
-var mail = document.createElement("a");
-var button = document.querySelector(".test");
-button = document.addEventListener("click", sendMail);
-*/
-
-const image = document.querySelector(".project");
-const modal = document.querySelector(".project__text");
-
-const openModal = function () {
-  modal.classList.remove("hidden");
-  image.classList.add("img__scaledown");
+  modals.classList.remove("hidden");
+  images.classList.add("img__scaledown");
 };
 
 const closeModal = function () {
-  modal.classList.add("hidden");
-  image.classList.remove("img__scaledown");
+  modals.classList.add("hidden");
+  images.classList.remove("img__scaledown");
 };
 
 const imageClicked = function () {
-  if (!image.classList.contains("img__scaledown")) {
+  if (!images.classList.contains("img__scaledown")) {
     openModal();
   } else {
     closeModal();
   }
 };
 
-modal.addEventListener("click", closeModal);
+modals.addEventListener("click", closeModal);
 
-image.addEventListener("click", imageClicked);
+images.addEventListener("click", imageClicked);
+/*
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", imageClicked);
+}
+for (let i = 0; i < modals.length; i++) {
+  modals[i].addEventListener("click", closeModal);
+}
+*/
