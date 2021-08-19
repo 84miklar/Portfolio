eb06272463fa50ab8a833b7827b2854dcb78c09f
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-08-19 15:46:44 +0200

    before changing pictures

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,101 +1,111 @@
-"use strict";
+"use strict"
 
 /*     PROJECTS      */
-const showroowImg = document.querySelector(".showroom__img");
-const showroomTxt = document.querySelector(".showroom__text");
-const calcImg = document.querySelector(".calc__img");
-const calcTxt = document.querySelector(".calc__text");
-const adventureImg = document.querySelector(".advent__img");
-const adventureTxt = document.querySelector(".advent__text");
+const showroowImg = document.querySelector(".showroom__img")
+const showroomTxt = document.querySelector(".showroom__text")
+const calcImg = document.querySelector(".calc__img")
+const calcTxt = document.querySelector(".calc__text")
+const adventureImg = document.querySelector(".advent__img")
+const adventureTxt = document.querySelector(".advent__text")
 
 const toggleProject = function (image, text) {
   if (
     document.querySelector(`.${image}`).classList.contains("img__scaledown")
   ) {
-    document.querySelector(`.${image}`).classList.remove("img__scaledown");
-    document.querySelector(`.${text}`).classList.add("hidden");
+    document.querySelector(`.${image}`).classList.remove("img__scaledown")
+    document.querySelector(`.${text}`).classList.add("hidden")
   } else {
-    document.querySelector(`.${image}`).classList.add("img__scaledown");
-    document.querySelector(`.${text}`).classList.remove("hidden");
+    document.querySelector(`.${image}`).classList.add("img__scaledown")
+    document.querySelector(`.${text}`).classList.remove("hidden")
   }
-};
+}
 
 /*       TRY MY GAME-BUTTON       */
-const gameButton = document.querySelector(".game__button");
+const gameButton = document.querySelector(".game__button")
+let life = ["X", "X", "X"]
 
 let toggleGame = function () {
   if (gameButton.textContent == "close game") {
-    document.querySelector(".game__button").textContent = "try my little game";
-    document.querySelector(".game").classList.add("hidden");
+    document.querySelector(".game__button").textContent = "try my little game"
+    document.querySelector(".game").classList.add("hidden")
   } else {
-    document.querySelector(".game__button").textContent = "close game";
-    document.querySelector(".game").classList.remove("hidden");
+    document.querySelector(".game__button").textContent = "close game"
+    document.querySelector(".game").classList.remove("hidden")
   }
 
-  document.querySelector(".numOne").textContent = "?";
-  document.querySelector(".calcType").textContent = setCalcType();
-  document.querySelector(".numTwo").textContent = "?";
-  pointsNumber.textContent = "0";
-  document.querySelector(".face").textContent = "😑";
-};
+  document.querySelector(".numOne").textContent = "?"
+  document.querySelector(".calcType").textContent = setCalcType()
+  document.querySelector(".numTwo").textContent = "?"
+  pointsNumber.textContent = "0"
+  document.querySelector(".face").textContent = "😑"
+  document.querySelector(".start").classList.remove("hidden")
+  guess.classList.add("hidden")
+  document.querySelector(".entered__Number").value = ""
+  life = ["🦄", "🦄", "🦄"]
+  document.querySelector(".life").textContent = life
+}
 
 /*         THE GAME             */
 
-let calcType = document.querySelector(".calcType");
-let guess = document.querySelector(".guess");
-let pointsNumber = document.querySelector(".points__number");
-let secretNumber = 0;
-let numberOne = 0;
-let numberTwo = 0;
-let calculationType = 0;
-let points = 0;
+let calcType = document.querySelector(".calcType")
+let guess = document.querySelector(".guess")
+let pointsNumber = document.querySelector(".points__number")
+let answer = Number(document.querySelector(".entered__Number").value)
+let secretNumber = 0
+let numberOne = 0
+let numberTwo = 0
+let calculationType = 0
+let points = 0
 
+//Sets the right calculation type depending on a random number
 let setCalcType = function () {
   if (calculationType <= 5) {
-    return (calcType.innerHTML = "+");
+    return (calcType.innerHTML = "+")
   } else {
-    return (calcType.innerHTML = "-");
+    return (calcType.innerHTML = "-")
   }
-};
+}
 
 const setNewNumbers = function () {
-  numberOne = Math.trunc(Math.random() * 10) + 1;
-  numberTwo = Math.trunc(Math.random() * 2) + 1;
-  calculationType = Math.trunc(Math.random() * 10) + 1;
-  document.querySelector(".numOne").textContent = numberOne;
-  document.querySelector(".calcType").textContent = setCalcType();
-  document.querySelector(".numTwo").textContent = numberTwo;
-};
+  numberOne = Math.trunc(Math.random() * 1000) + 1
+  numberTwo = Math.trunc(Math.random() * 1000) + 1
+  calculationType = Math.trunc(Math.random() * 10) + 1
+  document.querySelector(".numOne").textContent = numberOne
+  document.querySelector(".calcType").textContent = setCalcType()
+  document.querySelector(".numTwo").textContent = numberTwo
+}
 
+//Returns the correct answer
 let calculator = function () {
-  if (calcType.innerHTML === "+") return numberOne + numberTwo;
-  else return numberOne - numberTwo;
-};
+  if (calcType.innerHTML === "+") return numberOne + numberTwo
+  else return numberOne - numberTwo
+}
 
+//change text on start button and get new numbers
 document.querySelector(".start").addEventListener("click", function () {
-  document.querySelector(".start").textContent = "new numbers";
-  setNewNumbers();
-});
+  document.querySelector(".start").classList.add("hidden")
+  guess.classList.remove("hidden")
+  setNewNumbers()
+})
 
+//Decides if the guessed answer is same as correct answer, and adds points or reduces life
 guess.addEventListener("click", function () {
-  let answer = Number(document.querySelector(".entered__Number").value);
-  secretNumber = calculator();
-  if (answer === secretNumber) {
-    points++;
-    pointsNumber.textContent = `${points}`;
-    document.querySelector(".face").textContent = "😁";
-  } else {
-    document.querySelector(".face").textContent = "😞";
-  }
-  document.querySelector(".entered__Number").value = "";
-  setNewNumbers();
-});
+  secretNumber = calculator()
 
-// var sec = 0;
-// function pad(val) {
-//   return val > 9 ? val : "0" + val;
-// }
-// setInterval(function () {
-//   document.getElementById("seconds").innerHTML = pad(++sec % 60);
-//   document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
-// }, 1000);
+  if (life.length > 1) {
+    if (answer === secretNumber) {
+      points++
+      pointsNumber.textContent = `${points}`
+      document.querySelector(".face").textContent = "😁"
+    } else {
+      life.pop()
+      document.querySelector(".life").innerText = life
+      document.querySelector(".face").textContent = "😞"
+    }
+    document.querySelector(".entered__Number").value = ""
+    setNewNumbers()
+  } else {
+    document.querySelector(".life").innerHTML = "Game Over"
+    document.querySelector(".face").textContent = "😭"
+  }
+})
