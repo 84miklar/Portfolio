e1903c0ab5de9cf469ec31a70a0084e9e68e6b19
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-08-20 11:50:14 +0200

    fixed the game bug

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,6 +8,7 @@ const calcTxt = document.querySelector(".calc__text")
 const adventureImg = document.querySelector(".advent__img")
 const adventureTxt = document.querySelector(".advent__text")
 
+//Toggles classes for animation
 const toggleProject = function (image, text) {
   if (document.querySelector(`.${image}`).classList.contains("img--clicked")) {
     document.querySelector(`.${image}`).classList.remove("img--clicked")
@@ -22,6 +23,7 @@ const toggleProject = function (image, text) {
 const gameButton = document.querySelector(".game__button")
 let life = ["X", "X", "X"]
 
+//Toggles classes för animation
 let toggleGame = function () {
   if (gameButton.textContent == "close game") {
     document.querySelector(".game__button").textContent = "try my little game"
@@ -31,6 +33,11 @@ let toggleGame = function () {
     document.querySelector(".game").classList.add("game--open")
   }
 
+  resetGame()
+}
+
+// Resets the game to default
+let resetGame = () => {
   document.querySelector(".numOne").textContent = "?"
   document.querySelector(".calcType").textContent = setCalcType()
   document.querySelector(".numTwo").textContent = "?"
@@ -48,8 +55,8 @@ let toggleGame = function () {
 let calcType = document.querySelector(".calcType")
 let guess = document.querySelector(".guess")
 let pointsNumber = document.querySelector(".points__number")
-let answer = Number(document.querySelector(".entered__Number").value)
-let secretNumber = 0
+let answer = document.querySelector(".entered__Number")
+
 let numberOne = 0
 let numberTwo = 0
 let calculationType = 0
@@ -64,13 +71,15 @@ let setCalcType = function () {
   }
 }
 
+//Sets new numbers and calculation type for the game
 const setNewNumbers = function () {
-  numberOne = Math.trunc(Math.random() * 1000) + 1
-  numberTwo = Math.trunc(Math.random() * 1000) + 1
+  numberOne = Math.trunc(Math.random() * 10) + 1
+  numberTwo = Math.trunc(Math.random() * 5) + 1
   calculationType = Math.trunc(Math.random() * 10) + 1
   document.querySelector(".numOne").textContent = numberOne
   document.querySelector(".calcType").textContent = setCalcType()
   document.querySelector(".numTwo").textContent = numberTwo
+  document.querySelector(".entered__Number").value = ""
 }
 
 //Returns the correct answer
@@ -78,6 +87,7 @@ let calculator = function () {
   if (calcType.innerHTML === "+") return numberOne + numberTwo
   else return numberOne - numberTwo
 }
+let secretNumber = calculator()
 
 //change text on start button and get new numbers
 document.querySelector(".start").addEventListener("click", function () {
@@ -88,22 +98,34 @@ document.querySelector(".start").addEventListener("click", function () {
 
 //Decides if the guessed answer is same as correct answer, and adds points or reduces life
 guess.addEventListener("click", function () {
-  secretNumber = calculator()
-
   if (life.length > 1) {
-    if (answer === secretNumber) {
-      points++
-      pointsNumber.textContent = `${points}`
-      document.querySelector(".face").textContent = "😁"
+    if (answer.value == calculator()) {
+      correctAnswer()
     } else {
-      life.pop()
-      document.querySelector(".life").innerText = life
-      document.querySelector(".face").textContent = "😞"
+      wrongAnswer()
     }
-    document.querySelector(".entered__Number").value = ""
     setNewNumbers()
   } else {
-    document.querySelector(".life").innerHTML = "Game Over"
-    document.querySelector(".face").textContent = "😭"
+    gameOver()
   }
 })
+
+let gameOver = () => {
+  document.querySelector(".life").innerHTML = "Game Over"
+  document.querySelector(".face").textContent = "😭"
+}
+
+let correctAnswer = () => {
+  points++
+  pointsNumber.textContent = `${points}`
+  document.querySelector(".face").textContent = "😁"
+}
+
+let wrongAnswer = () => {
+  life.pop()
+  document.querySelector(".life").innerText = life
+  document.querySelector(".face").textContent = "😞"
+  if (life.length == 0) {
+    gameOver()
+  }
+}
